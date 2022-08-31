b00b93e43797805996f19ff9b4ca78f38f835a99
Mohamed Alaa <mohamed1998.alaa@yahoo.com@yahoo.com>
2022-08-31 20:46:02 +0200

    add mario2 function for merrored pyramid

--- Lab1/marioPyramid.py
+++ Lab1/marioPyramid.py
@@ -2,4 +2,9 @@ def marioPyramid(n):
     for i in range(1,n+1):
         print("*"*i)
 
-marioPyramid(4)
+def marioPyramid2(n):
+    j=1
+    for i in range(n+1,1,-1):   
+        print(" "*i+"*"*j)
+        j+=1
+marioPyramid2(4)
